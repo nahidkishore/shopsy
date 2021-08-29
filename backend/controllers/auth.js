f82acb86fca6f809c10fc5c1d7f1ff45ce4aed4b
Nahidul Islam <nahidkishore99.com>
2021-08-29 20:04:42 +0600

    register login api created

---
+++ backend/controllers/auth.js
@@ -0,0 +1,44 @@
+const User = require('../models/user');
+exports.signup = (req, res) => {
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (user)
+      return res.status(400).json({
+        message: 'User already registered',
+      });
+    const { firstName, lastName, email, password } = req.body;
+    const _user = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      username: Math.random().toString(),
+    });
+    _user.save((err, data) => {
+      if (err) {
+        return res.status(400).json({ message: 'Something went wrong' });
+      }
+      if (data) {
+        return res.status(201).json({
+          message: 'User created successfully',
+        });
+      }
+    });
+  });
+};
+// user signin
+
+exports.signin = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .exec((err, user) => {
+      if (err) return res.status(400).json({ err });
+      
+      if (user) {
+        if (user.authenticated(req.body.password)) {
+        
+      }
+      } else {
+        return res.status(400).json({ message: 'Something went wrong' }) 
+    }
+
+  });
+}
---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const env = require('dotenv');
+const colors = require('colors');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//routes
+const authRoutes = require('./routes/auth');
+env.config();
+
+//MongoDB connection
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.jolmh.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+.catch ((err) => console.error('Database  Connection Failed!'));
+
+app.use(bodyParser());
+app.use('/api', authRoutes);
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Hello from server side',
+  });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server is running on port ${PORT}`.cyan.bold));
