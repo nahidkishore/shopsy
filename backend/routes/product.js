bb370f4172b07964266f421c57d5f1e129511ec8
Nahidul Islam <nahidkishore99.com>
2021-09-06 02:30:09 +0600

    create product api

---
+++ backend/routes/product.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { createProduct } = require('../controllers/product');
+//const { addCategory, getCategories } = require('../controllers/category');
+const { requireSignIn, adminMiddleware } = require('../middleware');
+const multer = require('multer');
+const router = express.Router();
+const shortid = require('shortid');
+const path = require('path');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+    //cb(null, path.join(path.dirname(__dirname),'uploads'))
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post(
+  '/product/create',
+  requireSignIn,
+  adminMiddleware,
+  upload.array('productPicture'),
+  createProduct
+);
+//router.get('/category/getCategory', getCategories);
+module.exports = router;
