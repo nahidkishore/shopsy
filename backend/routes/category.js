ce6770dad716842869cad8ac480d1f03a6e6377a
Nahidul Islam <nahidkishore99.com>
2021-09-10 02:05:30 +0600

    categorImage add in category list

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -3,6 +3,20 @@ const { addCategory, getCategories } = require('../controllers/category');
 const { requireSignIn, adminMiddleware } = require('../middleware');
 const router = express.Router();
 
-router.post('/category/create', requireSignIn, adminMiddleware, addCategory);
+const shortid = require('shortid');
+const path = require('path');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+    //cb(null, path.join(path.dirname(__dirname),'uploads'))
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+router.post('/category/create', requireSignIn, adminMiddleware, upload.single('categoryImage'), addCategory);
 router.get('/category/getCategory', getCategories);
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const env = require('dotenv');
 const colors = require('colors');
 const app = express();
 const mongoose = require('mongoose');
-
+const path = require('path');
 //routes
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
@@ -28,6 +28,7 @@ mongoose
   .catch((error) => console.log(error));
 
 app.use(express.json());
+app.use('/public',express.static(path.join(__dirname, 'uploads')))
 app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
