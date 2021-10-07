25c3fb2409d0cc2ca05c3d04ae6711260f68f1d6
Nahidul Islam <nahidkishore99.com>
2021-10-08 02:12:16 +0600

    category list show in dropdown

---
+++ frontend/frontend-app/src/containers/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header/Header'
+import MenuHeader from '../MenuHeader/MenuHeader'
+const HomePage = () => {
+  return (
+    <div>
+      <Header></Header>
+      <MenuHeader/>
+    </div>
+  );
+};
+
+export default HomePage;
